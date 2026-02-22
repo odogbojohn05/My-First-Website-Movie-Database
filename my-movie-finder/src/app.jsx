4b738aaf622e33a4ec4490b9dff7ae00eae2f620
odogbojohn05 <ebubeodogbo@email.com>
2026-02-22 18:00:43 +0100

    updated file

--- my-movie-finder/src/app.jsx
+++ my-movie-finder/src/app.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import Search from "./components/Search";
 
 function App() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4 text-center">Movie Finder</h1>
+      <h1 className="text-3xl font-bold mb-4 text-center text-blue-600">
+        Movie Finder
+      </h1>
       <Search />
     </div>
   );
