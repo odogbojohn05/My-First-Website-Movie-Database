d6e9ad16432bcfb0e982d50b57185b5f86f4ab5c
odogbojohn05 <ebubeodogbo@email.com>
2026-02-26 00:46:19 +0100

    updated file

--- my-movie-finder/src/app.jsx
+++ my-movie-finder/src/app.jsx
@@ -1,6 +1,5 @@
-// src/App.jsx
 import React, { useState } from "react";
-import Search from "./Search";
+import Search from "./components/Search";   
 import MovieList from "./components/MovieList";
 
 function App() {
@@ -8,19 +7,23 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
+    
       <header className="flex items-center mb-6">
         <span className="text-3xl mr-2">🎬</span>
         <h1 className="text-3xl font-bold text-blue-600">MovieFinder</h1>
       </header>
 
+    
       <Search onSearch={setQuery} />
 
+      
       {!query && (
         <p className="text-gray-600 mt-6">
           Search for movies above to get started.
         </p>
       )}
 
+      
       {query && <MovieList query={query} />}
     </div>
   );
