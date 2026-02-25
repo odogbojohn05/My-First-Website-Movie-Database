b62665d5f74ef2fc61d1eb817fe1698d85ece584
odogbojohn05 <ebubeodogbo@email.com>
2026-02-25 22:34:27 +0100

    updated app.jsx file

--- my-movie-finder/src/app.jsx
+++ my-movie-finder/src/app.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Landing from "./components/Landing";
 import Search from "./components/Search";
 
 function App() {
+  const [started, setStarted] = useState(false);
+
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4 text-center text-blue-600">
-        Movie Finder
-      </h1>
-      <Search />
+      {!started ? (
+        <Landing onStart={() => setStarted(true)} />
+      ) : (
+        <div>
+          <h1 className="text-3xl font-bold mb-4 text-center">Movie Finder</h1>
+          <Search />
+        </div>
+      )}
     </div>
   );
 }
