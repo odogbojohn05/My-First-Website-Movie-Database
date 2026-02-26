f2948e9431ddb190cd0e27eaab2ed7606a153795
odogbojohn05 <ebubeodogbo@email.com>
2026-02-26 01:38:43 +0100

    updated file

--- my-movie-finder/src/services/movieService.js
+++ my-movie-finder/src/services/movieService.js
@@ -1,5 +1,6 @@
 const API_KEY = import.meta.env.VITE_OMDB_KEY;
 
+console.log("OMDb API Key:", API_KEY);
 
 export async function searchMovies(query) {
   if (!API_KEY) {
@@ -7,10 +8,12 @@ export async function searchMovies(query) {
   }
 
   const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(query)}`;
+  console.log("Fetching:", url);
 
   try {
     const res = await fetch(url);
     const data = await res.json();
+    console.log("Response:", data); 
 
     if (data.Response === "True") {
       return data.Search; 
@@ -19,5 +22,25 @@ export async function searchMovies(query) {
     }
   } catch (err) {
     throw new Error("Failed to fetch movies: " + err.message);
+  }
+}
+
+
+export async function getMovieDetails(id) {
+  const url = `https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}&plot=full`;
+  console.log("Fetching details:", url);
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log("Details Response:", data);
+
+    if (data.Response === "True") {
+      return data;
+    } else {
+      throw new Error(data.Error || "Movie details not found");
+    }
+  } catch (err) {
+    throw new Error("Failed to fetch movie details: " + err.message);
   }
 }
