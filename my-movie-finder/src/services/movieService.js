aecba70f2e14d5994b2aa7beaa0e3bdd805c80f8
odogbojohn05 <ebubeodogbo@email.com>
2026-02-26 01:24:19 +0100

    updated file

--- my-movie-finder/src/services/movieService.js
+++ my-movie-finder/src/services/movieService.js
@@ -1,13 +1,23 @@
 const API_KEY = import.meta.env.VITE_OMDB_KEY;
 
+
 export async function searchMovies(query) {
-  const res = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(query)}`
-  );
-  const data = await res.json();
-  if (data.Response === "True") {
-    return data.Search; 
-  } else {
-    throw new Error(data.Error || "Movie not found");
+  if (!API_KEY) {
+    throw new Error("API key is missing. Check your .env file.");
+  }
+
+  const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(query)}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (data.Response === "True") {
+      return data.Search; 
+    } else {
+      throw new Error(data.Error || "Movie not found");
+    }
+  } catch (err) {
+    throw new Error("Failed to fetch movies: " + err.message);
   }
 }
